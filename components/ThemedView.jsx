935115c71078c47884fa0be0919eb2d2a387f56f
udeshika123cyber <udeshikaprabhashini21@gmail.com>
2025-06-20 10:01:06 +0530

    modified

--- components/ThemedView.jsx
+++ components/ThemedView.jsx
@@ -1,16 +1,34 @@
 import { View, useColorScheme } from "react-native";
 import React from "react";
 import { Colors } from "../constants/Colors";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
-const ThemedView = ({ style, ...props }) => {
+const ThemedView = ({ style, safe = false, ...props }) => {
   const colorScheme = useColorScheme();
   // const theme = Colors[colorScheme] ?? Colors.light;
   const theme = Colors[colorScheme ?? "light"] ?? Colors.light;
+  if (!safe)
+    return (
+      <View
+        style={[
+          {
+            backgroundColor: theme.background,
+          },
+          style,
+        ]}
+        {...props}
+      />
+    );
+  // If safe is true, we assume the view is used inside a SafeAreaView
+  const insets = useSafeAreaInsets();
+
   return (
     <View
       style={[
         {
           backgroundColor: theme.background,
+          paddingTop: insets.top,
+          paddingBottom: insets.bottom,
         },
         style,
       ]}
